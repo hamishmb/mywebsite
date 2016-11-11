e4aef13a3b05e1ffff4204ab4bef1b043ae7b2c7
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2016-11-11 12:11:50 +0000

    Add Wine Autostart v2.0.2 to downloads.

--- html/Downloads/wineautostart.php
+++ html/Downloads/wineautostart.php
@@ -18,15 +18,11 @@
         <div id="MainContent">
             <h1>Downloads For Wine Autostart</h1>
             <p>
-            This is a maintenance release that fixes just one very occasional but annoying bug in v2.0, where Wine Autostart may prompt you to check a disk for software twice, or three times. The issue was actually a weird race condition when stopping the backend thread.<br><br>
+            This is a minor update to Wine Autostart that fixes several important bugs, adds a few helpful features, and makes the GUI easier to use.<br><br>
 
-            Other than that fix, this release is identical to the last one.<br><br>
+            Most importantly, configuration is saved separately for each user, so it doesn't ask for the password all the time any more, and each user can set Wine Autostart up differently to their taste.<br><br>
 
-            I hope you enjoy using this program, and you can check out my other packages at www.launchpad.net/~hamishmb/+related-projects<br><br>
-
-            UPDATE 17/4/2016: There's now a package for Ubuntu 16.04 (Xenial Xerus).<br><br>
-
-            UPDATE 18/10/2016: There's now a package for Ubuntu 16.10 (Yakkety Yak).<br><br>
+            There are also a few performance improvements behind the scenes so you can get to running your software even faster.
             </p>
             <table>
                 <caption><h2>Download Files</h2></caption>
@@ -38,32 +34,32 @@
                 </tr>
                 <tr>
                     <td><img src="/files/Icons/Ubuntu_logo_copyleft_1.svg" width="40" height="40" alt="Copyleft Ubuntu Logo"></td>
-                    <td>Wine Autostart v2.0.1 For Ubuntu 16.10</td>
-                    <td><a href="/files/Downloads/wineautostart/2.0.1/Yakkety/wineautostart_2.0.1yakkety-0ubuntu1~ppa1_all.deb">All Systems</a></td>
+                    <td>Wine Autostart v2.0.2 For Ubuntu 16.10</td>
+                    <td><a href="/files/Downloads/wineautostart/2.0.2/Yakkety/wineautostart_2.0.2yakkety-0ubuntu1~ppa1_all.deb">All Systems</a></td>
                     <td>***TODO***</td>
                 </tr>
                 <tr>
                     <td><img src="/files/Icons/Ubuntu_logo_copyleft_1.svg" width="40" height="40" alt="Copyleft Ubuntu Logo"></td>
-                    <td>Wine Autostart v2.0.1 For Ubuntu 16.04 LTS</td>
-                    <td><a href="/files/Downloads/wineautostart/2.0.1/Xenial/wineautostart_2.0.1xenial-0ubuntu1~ppa1_all.deb">All Systems</a></td>
+                    <td>Wine Autostart v2.0.2 For Ubuntu 16.04 LTS</td>
+                    <td><a href="/files/Downloads/wineautostart/2.0.2/Xenial/wineautostart_2.0.2xenial-0ubuntu1~ppa1_all.deb">All Systems</a></td>
                     <td>***TODO***</td>
                 </tr>
                 <tr>
                     <td><img src="/files/Icons/Ubuntu_logo_copyleft_1.svg" width="40" height="40" alt="Copyleft Ubuntu Logo"></td>
-                    <td>Wine Autostart v2.0.1 For Ubuntu 14.04 LTS</td>
-                    <td><a href="/files/Downloads/wineautostart/2.0.1/Trusty/wineautostart_2.0.1trusty-0ubuntu1~ppa1_all.deb">All Systems</a></td>
+                    <td>Wine Autostart v2.0.2 For Ubuntu 14.04 LTS</td>
+                    <td><a href="/files/Downloads/wineautostart/2.0.2/Trusty/wineautostart_2.0.2trusty-0ubuntu1~ppa1_all.deb">All Systems</a></td>
                     <td>***TODO***</td>
                 </tr>
                 <tr>
                     <td><img src="/files/Icons/Ubuntu_logo_copyleft_1.svg" width="40" height="40" alt="Copyleft Ubuntu Logo"></td>
-                    <td>Wine Autostart v2.0.1 For Ubuntu 12.04 LTS</td>
-                    <td><a href="/files/Downloads/wineautostart/2.0.1/Precise/wineautostart_2.0.1precise-0ubuntu1~ppa1_all.deb">All Systems</a></td>
+                    <td>Wine Autostart v2.0.2 For Ubuntu 12.04 LTS</td>
+                    <td><a href="/files/Downloads/wineautostart/2.0.2/Precise/wineautostart_2.0.2precise-0ubuntu1~ppa1_all.deb">All Systems</a></td>
                     <td>***TODO***</td>
                 </tr>
                 <tr>
                     <td><img src="/files/Icons/Linux_logo.jpg" width="34" height="40" alt="Linux Logo"></td>
-                    <td>Wine Autostart v2.0.1 For Other Linux Distributions</td>
-                    <td><a href="/files/Downloads/wineautostart/2.0.1/Otherdistro/wineautostart_2.0.1otherdistro-0ubuntu1~ppa1.tar.gz">All Systems</a></td>
+                    <td>Wine Autostart v2.0.2 For Other Linux Distributions</td>
+                    <td><a href="/files/Downloads/wineautostart/2.0.2/Otherdistro/wineautostart_2.0.2otherdistro-0ubuntu1~ppa1.tar.gz">All Systems</a></td>
                     <td>***TODO***</td>
                 </tr>
             </table>
--- html/Museum/wineautostart.php
+++ html/Museum/wineautostart.php
@@ -32,6 +32,7 @@
                     <li>
                         <a href="#2.0Series">2.0 Series</a>
                         <ol>
+                            <li><a href="#2.0.1">2.0.1 (Stable)</a></li>
                             <li><a href="#2.0">2.0 (Stable)</a></li>
                             <li><a href="#2.0rc1">2.0~rc1 (Development)</a></li>
                         </ol>
@@ -61,6 +62,60 @@
             <!-- Heights are in , but use digits only -->
             <section>
                 <h2 id="2.0Series">Wine Autostart 2.0 Series</h2>
+                <article>
+                    <h3 id="2.0.1">Wine Autostart v2.0.1</h3>
+                    <p>
+                    This is a maintenance release that fixes just one very occasional but annoying bug in v2.0, where Wine Autostart may prompt you to check a disk for software twice, or three times. The issue was actually a weird race condition when stopping the backend thread.<br><br>
+
+                    Other than that fix, this release is identical to the last one.<br><br>
+
+                    I hope you enjoy using this program, and you can check out my other packages at www.launchpad.net/~hamishmb/+related-projects<br><br>
+
+                    UPDATE 17/4/2016: There's now a package for Ubuntu 16.04 (Xenial Xerus).<br><br>
+
+                    UPDATE 18/10/2016: There's now a package for Ubuntu 16.10 (Yakkety Yak).<br><br>
+                    </p>
+                    <table>
+                        <caption><h2>Download Files</h2></caption>
+                        <tr>
+                            <th>Icon</th>
+                            <th>Description</th>
+                            <th>Download</th>
+                            <th>No. Of Downloads</th>
+                        </tr>
+                        <tr>
+                            <td><img src="/files/Icons/Ubuntu_logo_copyleft_1.svg" width="40" height="40" alt="Copyleft Ubuntu Logo"></td>
+                            <td>Wine Autostart v2.0.1 For Ubuntu 16.10</td>
+                            <td><a href="/files/Downloads/wineautostart/2.0.1/Yakkety/wineautostart_2.0.1yakkety-0ubuntu1~ppa1_all.deb">All Systems</a></td>
+                            <td>***TODO***</td>
+                        </tr>
+                        <tr>
+                            <td><img src="/files/Icons/Ubuntu_logo_copyleft_1.svg" width="40" height="40" alt="Copyleft Ubuntu Logo"></td>
+                            <td>Wine Autostart v2.0.1 For Ubuntu 16.04 LTS</td>
+                            <td><a href="/files/Downloads/wineautostart/2.0.1/Xenial/wineautostart_2.0.1xenial-0ubuntu1~ppa1_all.deb">All Systems</a></td>
+                            <td>***TODO***</td>
+                        </tr>
+                        <tr>
+                            <td><img src="/files/Icons/Ubuntu_logo_copyleft_1.svg" width="40" height="40" alt="Copyleft Ubuntu Logo"></td>
+                            <td>Wine Autostart v2.0.1 For Ubuntu 14.04 LTS</td>
+                            <td><a href="/files/Downloads/wineautostart/2.0.1/Trusty/wineautostart_2.0.1trusty-0ubuntu1~ppa1_all.deb">All Systems</a></td>
+                            <td>***TODO***</td>
+                        </tr>
+                        <tr>
+                            <td><img src="/files/Icons/Ubuntu_logo_copyleft_1.svg" width="40" height="40" alt="Copyleft Ubuntu Logo"></td>
+                            <td>Wine Autostart v2.0.1 For Ubuntu 12.04 LTS</td>
+                            <td><a href="/files/Downloads/wineautostart/2.0.1/Precise/wineautostart_2.0.1precise-0ubuntu1~ppa1_all.deb">All Systems</a></td>
+                            <td>***TODO***</td>
+                        </tr>
+                        <tr>
+                            <td><img src="/files/Icons/Linux_logo.jpg" width="34" height="40" alt="Linux Logo"></td>
+                            <td>Wine Autostart v2.0.1 For Other Linux Distributions</td>
+                            <td><a href="/files/Downloads/wineautostart/2.0.1/Otherdistro/wineautostart_2.0.1otherdistro-0ubuntu1~ppa1.tar.gz">All Systems</a></td>
+                            <td>***TODO***</td>
+                        </tr>
+                    </table>
+                    <br>
+                    <p class="BackToTop"><a href="#navigation">Back To Top</a></p>
                 <article>
                     <h3 id="2.0">Wine Autostart v2.0</h3>
                     <p>
