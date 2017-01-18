86f27fa27f9d0277b220b557e86e0e24eb2ead05
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2017-01-18 12:39:58 +0000

    Finish counter implementation, and put some safety checks in.

--- html/thankyou.php
+++ html/thankyou.php
@@ -12,6 +12,12 @@
         <?php $GLOBALS["CURRENTPAGE"] = 'Home'; ?>
     </head>
     <body>
+        <!-- Safety Check - Do the download file and counter exist? -->
+        <?php
+            $junk = file_exists($BASEDIR . $_GET['file']) or die("Invalid request.");
+            $junk = file_exists($BASEDIR . $_GET['file'] . ".counter") or die("Invalid request.");
+        ?>
+
         <!-- Navigation Between Pages -->
         <?php include_once $INCLUDESDIR . 'mainnavigation.php' ; ?>
         <!-- Content of the Page -->
@@ -21,15 +27,31 @@
             Your file should begin downloading automatically. If it doesn't, start the download manually by clicking <a href=<?php echo("'" . $_GET['file'] . "'")?>>here.</a><br>
             To go back to the page you were on before, click <a href=<?php echo("'" . $_GET['prevpage'] . "'")?>>here.</a> To go to the home page, click <a href="/html/index.php">here.</a>
             </p>
+
             <!-- Provide the file after a delay of 1 second -->
             <script type="text/javascript">
                 window.onload = setTimeout(function(){
                 document.location = <?php echo("'" . $_GET['file'] . "'") ?>;
                 },1000)
             </script>
-
         </div>
         <!-- Footer -->
         <?php include_once $INCLUDESDIR . 'footer.html' ; ?>
+        <!-- Update the counter -->
+        <!-- Read counter value -->
+        <?php
+            $counter = fopen($BASEDIR . $_GET['file'] . ".counter", "r") or die("Unable to update counter!");
+            $NumberOfDownloads = (int)fread($counter,filesize($BASEDIR . $_GET['file'] . ".counter"));
+            fclose($counter);
+        ?>
+
+        <!-- Write new value to counter -->
+        <?php
+            $NumberOfDownloads = $NumberOfDownloads + 1;
+            $counter = fopen($BASEDIR . $_GET['file'] . ".counter", "w") or die("Unable to update counter!");
+            fwrite($counter, (string)$NumberOfDownloads);
+            fclose($counter);
+        ?>
+
     </body>
 </html>
